78dc018afb829cd8298fbb980b4316199c9d178e
Jamie <jamiechu9288@gmail.com>
2020-09-11 23:22:03 -0400

    Initial Commit

---
+++ jamie_chu_nike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jamie_chu_nike
+//
+//  Created by Jamie Chu on 9/11/20.
+//  Copyright © 2020 Jamie Chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
