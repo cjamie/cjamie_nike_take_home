477480062dd359995d3e541da29796a064c49820
Jamie <jamiechu9288@gmail.com>
2020-09-12 00:17:59 -0400

    added Nike_Network as a framework into project

---
+++ Nike_Network/Nike_Network/Nike_Network.h
@@ -0,0 +1,19 @@
+//
+//  Nike_Network.h
+//  Nike_Network
+//
+//  Created by Jamie Chu on 9/12/20.
+//  Copyright © 2020 Jamie Chu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Nike_Network.
+FOUNDATION_EXPORT double Nike_NetworkVersionNumber;
+
+//! Project version string for Nike_Network.
+FOUNDATION_EXPORT const unsigned char Nike_NetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nike_Network/PublicHeader.h>
+
+
