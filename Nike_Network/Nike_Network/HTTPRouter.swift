6793a430f3ca95aaad299605825a1c4584e897af
Jamie <jamiechu9288@gmail.com>
2020-09-12 20:34:49 -0400

    removed Any type from Router, removed LocalizableError conformance from NetworkingError, added conditional URLRequestConvertible implementation to HTTPRouter type

--- Nike_Network/Nike_Network/HTTPRouter.swift
+++ Nike_Network/Nike_Network/HTTPRouter.swift
@@ -8,16 +8,19 @@
 
 import Foundation
 
-protocol HTTPRouter: URLRequestConvertible {
+protocol HTTPRouter {
     var method: String { get } // this should not be an enum
     var host: String { get }
     var scheme: String { get }
     var path: String { get }
-    var parameters: [String: Any] { get }
-    var additionalHttpHeaders: [String: Any] { get }
+    var parameters: [String: String] { get }
+    var additionalHttpHeaders: [String: String] { get }
 }
 
 extension HTTPRouter {
+    
+    // MARK: - Public API
+    
     func baseURL() throws -> URL? {
             let condition = stringComponents.allSatisfy { !$0.isEmpty }
         
@@ -27,22 +30,49 @@ extension HTTPRouter {
             components.host = host
             components.scheme = scheme
             components.path = path
-            components.queryItems = parameters.map { .init(name: $0.key, value: "\($0.value)") }
+            components.queryItems = parameters.map { .init(name: $0.key, value: $0.value) }
     
             return components.url
     }
+    
+    // MARK: Helpers
     
     private var stringComponents: [String] {
         [host, scheme, path]
     }
 }
 
+extension HTTPRouter where Self: URLRequestConvertible {
+    func asURLRequest() throws -> URLRequest {
+        guard let baseURL = try? baseURL() else { throw NetworkingError.malformedURL }
+        var request = URLRequest(url: baseURL)
+        request.httpMethod = method
+        additionalHttpHeaders.forEach { header in
+            request.addValue(header.value, forHTTPHeaderField: header.key)
+        }
+        return request
+    }
+}
+
+
 protocol URLRequestConvertible {
     func asURLRequest() throws -> URLRequest
 }
 
-
-enum NetworkingError: Error, LocalizedError, Equatable {
+enum NetworkingError: Error, Equatable {
+    
+    case malformedURL
+    case malformedRequest
+    case swift(Error)
+    case noResponse
+    case badResponse(code: Int)
+    case noData
+    case noImage
+    case jsonDecoding(Error)
+    case serverError(String)
+    
+    // MARK: - Equatable
+    
     static func == (lhs: NetworkingError, rhs: NetworkingError) -> Bool {
         switch (lhs, rhs) {
         case (.malformedURL, .malformedURL),
@@ -61,45 +91,6 @@ enum NetworkingError: Error, LocalizedError, Equatable {
         default:
             return false
         }
-    }
-    
-    case malformedURL
-    case malformedRequest
-    case swift(Error)
-    case noResponse
-    case badResponse(code: Int)
-    case noData
-    case noImage
-    case jsonDecoding(Error)
-    case serverError(String)
-    // MARK: - LocalizedError
-    
-    var errorDescription: String? {
-        let description: String
-        
-        switch self {
-        case .malformedURL:
-            description = "Failed to create a URL"
-        case .malformedRequest:
-            description = "Failed to create a request"
-        case .noData:
-            description = "There is no data returned"
-        case .swift(let error):
-            description = error.localizedDescription
-        case .noResponse:
-            description = "No response"
-        case .badResponse(code: let code):
-            description = "Bad response code: \(code)"
-        case .noImage:
-            description = "No image"
-        case .jsonDecoding(let error):
-            description = "Json decoding error: \(error.localizedDescription)"
-        case .serverError(let errorString):
-            description = errorString
-            return errorString
-        }
-        
-        return "\(Self.self)" + ": " + description
     }
 }
 
@@ -141,14 +132,14 @@ enum ItunesRouter: HTTPRouter {
         }
     }
     
-    var parameters: [String : Any] {
+    var parameters: [String: String] {
         switch self {
         case .topAlbumsAcrossAllGenres:
             return [:]
         }
     }
     
-    var additionalHttpHeaders: [String : Any] {
+    var additionalHttpHeaders: [String : String] {
         switch self {
         case .topAlbumsAcrossAllGenres(let count):
             return [:]
