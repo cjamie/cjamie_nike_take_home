e97c4bee2116cba988ed3e190c90e2f55619c4b1
Jamie <jamiechu9288@gmail.com>
2020-09-13 17:01:38 -0400

    converted strings to URLs and Dates, while preserving unit tests

--- Nike_Network/Nike_Network/POJOs/ItunesMonolith.swift
+++ Nike_Network/Nike_Network/POJOs/ItunesMonolith.swift
@@ -16,25 +16,25 @@ public struct ItunesMonolith: Codable, Equatable {
 // MARK: - Feed
 public struct Feed: Codable, Equatable {
     let title: String
-    let id: String
+    let id: String // this probably should not be url
     let author: Author
     let links: [Link]
     let copyright, country: String
-    let icon: String
-    let updated: String
+    let icon: URL
+    let updated: Date
     let results: [AlbumResult]
 }
 
 // MARK: - Author
 struct Author: Codable, Equatable {
     let name: String
-    let uri: String
+    let uri: URL // this can be url
 }
 
 // MARK: - Link
 struct Link: Codable, Equatable {
-    let linkSelf: String?
-    let alternate: String?
+    let linkSelf: URL? // this can be url
+    let alternate: URL? // this can be url
 
     enum CodingKeys: String, CodingKey {
         case linkSelf = "self"
@@ -46,13 +46,13 @@ struct Link: Codable, Equatable {
 struct AlbumResult: Codable, Equatable {
     let artistName, id, releaseDate, name: String
     let kind: Kind
-    let copyright: String?
+    let copyright: String
     let artistID: String
-    let contentAdvisoryRating: String? // TODO: - this should be an enum?
-    let artistURL: String
-    let artworkUrl100: String
+    let contentAdvisoryRating: String // TODO: - this should be an enum?
+    let artistURL: URL
+    let artworkUrl100: URL
     let genres: [Genre]
-    let url: String // TODO: - this should be decodable
+    let url: URL
 
     enum CodingKeys: String, CodingKey {
         case artistName, id, releaseDate, name, kind, copyright
@@ -67,7 +67,7 @@ struct AlbumResult: Codable, Equatable {
 struct Genre: Codable, Equatable {
     let genreID: String
     let name: Name
-    let url: String // TODO: - this should be decodable
+    let url: URL
 
     enum CodingKeys: String, CodingKey {
         case genreID = "genreId"
--- Nike_Network/Nike_NetworkTests/ItunesNetworkAPITests.swift
+++ Nike_Network/Nike_NetworkTests/ItunesNetworkAPITests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import Nike_Network
 class ItunesNetworkAPITests: XCTestCase {
 
-    func test_apiReturnExpectedInstance() {
+    func test_iTunesMonolithDecoding() {
         // GIVEN
         let sut = makeSUT()
         let myExpectation = expectation(description: #function)
@@ -30,18 +30,24 @@ class ItunesNetworkAPITests: XCTestCase {
             }
         }
         
-        wait(for: [myExpectation], timeout: 3)
+        wait(for: [myExpectation], timeout: 0.1)
         
         XCTAssertEqual(capturedMonolithInstance, expectedMonolith())
         
     }
-    
+        
     // MARK: - Helpers
     
     private func makeSUT(source: Source = .local) -> ItunesRecordFetcher {
         switch source {
         case .local:
-            return LocallyStubbedItunesAPI(decoder: .init(), data: privateStub)
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .formatted(customDateFormatter)
+            return LocallyStubbedItunesAPI(
+                decoder: decoder,
+                data: privateStub
+            )
         case .remote:
             return RemoteItunesAPI()
         }
@@ -76,29 +82,38 @@ class ItunesNetworkAPITests: XCTestCase {
         case remote
     }
     
+    private let customDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        return formatter
+    }()
+    
+    // TODO: - remove force unwraps on dates
     private func expectedMonolith() -> ItunesMonolith {
+        let updatedDate = customDateFormatter.date(from: "2020-09-12T01:49:37.000-07:00") ?? Date()
         return .init(
             feed: .init(
                 title: "Top Albums",
                 id: "https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/3/explicit.json",
                 author: .init(
                     name: "iTunes Store",
-                    uri: "http://wwww.apple.com/us/itunes/"
+                    uri: URL(string: "http://wwww.apple.com/us/itunes/")!
                 ),
                 links: [
                     .init(
-                        linkSelf: "https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/3/explicit.json",
+                        linkSelf: URL(string: "https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/3/explicit.json"),
                         alternate: nil
                     ),
                     .init(
                         linkSelf: nil,
-                        alternate: "https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewTop?genreId=34&popId=82&app=music"
+                        alternate: URL(string:  "https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewTop?genreId=34&popId=82&app=music")
                     )
                 ],
                 copyright: "Copyright © 2018 Apple Inc. All rights reserved.",
                 country: "us",
-                icon: "http://itunes.apple.com/favicon.ico",
-                updated: "2020-09-12T01:49:37.000-07:00", // TODO: -- this needs a special date decoder
+                icon: URL(string: "http://itunes.apple.com/favicon.ico")!,
+                updated: updatedDate, // TODO: -- this needs a special date decoder
                 results: [
                     .init(
                         artistName: "YoungBoy Never Broke Again",
@@ -109,20 +124,21 @@ class ItunesNetworkAPITests: XCTestCase {
                         copyright: "Never Broke Again, LLC / Atlantic Records, ℗ 2020 Artist Partner Group, Inc.",
                         artistID: "1168822104",
                         contentAdvisoryRating: "Explicit",
-                        artistURL: "https://music.apple.com/us/artist/youngboy-never-broke-again/1168822104?app=music",
-                        artworkUrl100: "https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/13/d6/e3/13d6e3ac-7a38-c6c3-6566-e027f3735426/075679802378.jpg/200x200bb.png",
+                        artistURL: URL(string: "https://music.apple.com/us/artist/youngboy-never-broke-again/1168822104?app=music")!,
+                        artworkUrl100: URL(string: "https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/13/d6/e3/13d6e3ac-7a38-c6c3-6566-e027f3735426/075679802378.jpg/200x200bb.png")!,
                         genres: [
                             .init(
                                 genreID: "18",
                                 name: .hipHopRap,
-                                url: "https://itunes.apple.com/us/genre/id18"),
+                                url: URL(string: "https://itunes.apple.com/us/genre/id18")!
+                            ),
                             .init(
                                 genreID: "34",
                                 name: .music,
-                                url: "https://itunes.apple.com/us/genre/id34"
+                                url: URL(string: "https://itunes.apple.com/us/genre/id34")!
                             )
                         ],
-                        url: "https://music.apple.com/us/album/top/1530122403?app=music"
+                        url: URL(string: "https://music.apple.com/us/album/top/1530122403?app=music")!
                     ),
                     .init(
                         artistName: "Big Sean",
@@ -133,21 +149,21 @@ class ItunesNetworkAPITests: XCTestCase {
                         copyright: "℗ 2020 Getting Out Our Dreams, Inc./Def Jam Recordings, a division of UMG Recordings, Inc.",
                         artistID: "302533564",
                         contentAdvisoryRating: "Explicit",
-                        artistURL: "https://music.apple.com/us/artist/big-sean/302533564?app=music",
-                        artworkUrl100: "https://is5-ssl.mzstatic.com/image/thumb/Music124/v4/46/b2/f1/46b2f125-58b9-e503-ad81-5174ffd06f3b/20UMGIM72128.rgb.jpg/200x200bb.png",
+                        artistURL: URL(string: "https://music.apple.com/us/artist/big-sean/302533564?app=music")!,
+                        artworkUrl100: URL(string: "https://is5-ssl.mzstatic.com/image/thumb/Music124/v4/46/b2/f1/46b2f125-58b9-e503-ad81-5174ffd06f3b/20UMGIM72128.rgb.jpg/200x200bb.png")!,
                         genres: [
                             .init(
                                 genreID: "18",
                                 name: .hipHopRap,
-                                url: "https://itunes.apple.com/us/genre/id18"
+                                url: URL(string: "https://itunes.apple.com/us/genre/id18")!
                             ),
                             .init(
                                 genreID: "34",
                                 name: .music,
-                                url: "https://itunes.apple.com/us/genre/id34"
+                                url: URL(string: "https://itunes.apple.com/us/genre/id34")!
                             )
                         ],
-                        url: "https://music.apple.com/us/album/detroit-2/1530247672?app=music"
+                        url: URL(string: "https://music.apple.com/us/album/detroit-2/1530247672?app=music")!
                     ),
                     .init(
                         artistName: "Pop Smoke",
@@ -158,21 +174,21 @@ class ItunesNetworkAPITests: XCTestCase {
                         copyright: "Victor Victor Worldwide; ℗ 2020 Republic Records, a division of UMG Recordings, Inc. & Victor Victor Worldwide",
                         artistID: "1450601383",
                         contentAdvisoryRating: "Explicit",
-                        artistURL: "https://music.apple.com/us/artist/pop-smoke/1450601383?app=music",
-                        artworkUrl100: "https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/17/6d/e0/176de0c9-42a6-8741-9d22-6aae00094e1d/20UMGIM55833.rgb.jpg/200x200bb.png",
+                        artistURL: URL(string: "https://music.apple.com/us/artist/pop-smoke/1450601383?app=music")!,
+                        artworkUrl100: URL(string: "https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/17/6d/e0/176de0c9-42a6-8741-9d22-6aae00094e1d/20UMGIM55833.rgb.jpg/200x200bb.png")!,
                         genres: [
                             .init(
                                 genreID: "18",
                                 name: .hipHopRap,
-                                url: "https://itunes.apple.com/us/genre/id18"
+                                url: URL(string: "https://itunes.apple.com/us/genre/id18")!
                             ),
                             .init(
                                 genreID: "34",
                                 name: .music,
-                                url: "https://itunes.apple.com/us/genre/id34"
+                                url: URL(string: "https://itunes.apple.com/us/genre/id34")!
                             ),
                         ],
-                        url: "https://music.apple.com/us/album/shoot-for-the-stars-aim-for-the-moon/1521889004?app=music"
+                        url: URL(string: "https://music.apple.com/us/album/shoot-for-the-stars-aim-for-the-moon/1521889004?app=music")!
                     ),
                 ]
             )
